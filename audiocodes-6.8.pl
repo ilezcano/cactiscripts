934c30d861e11b85b64c192d8f9723a6f98d45a9
Ismael <ish@dorcas.lezcano.org>
2014-12-03 10:27:40 -0500

    Changed OIDs

--- audiocodes-6.8.pl
+++ audiocodes-6.8.pl
@@ -20,9 +20,8 @@ die $error if $error;
 
 my $ipGroupDescription                = '1.3.6.1.4.1.5003.9.10.3.1.1.23.21.1.6';
 my $ipGroupSIPGroupName               = '1.3.6.1.4.1.5003.9.10.3.1.1.23.21.1.8';
-my $acPMSIPIPGroupInviteDialogsTotal  = '1.3.6.1.4.1.5003.10.8.2.52.22.1.10';
-my $acPMSIPIPGroupInInviteDialogsTotal  = '1.3.6.1.4.1.5003.10.8.2.52.32.1.10';
-my $acPMSIPIPGroupOutInviteDialogsTotal  = '1.3.6.1.4.1.5003.10.8.2.52.35.1.10';
+my $acPMSIPIPGroupInDialogsTotal      = '1.3.6.1.4.1.5003.10.8.2.52.30.1.3';
+my $acPMSIPIPGroupOutDialogsTotal     = '1.3.6.1.4.1.5003.10.8.2.52.31.1.3';
 
 die $error if $error;
 
@@ -35,9 +34,8 @@ foreach my $index (@indexes)
 	print "Index $index ";
 
 	my $test = $session->get_request(-varbindlist => ["$ipGroupSIPGroupName.$index",
-		"$acPMSIPIPGroupInviteDialogsTotal.$index.0",
-		"$acPMSIPIPGroupInInviteDialogsTotal.$index.0",
-		"$acPMSIPIPGroupOutInviteDialogsTotal.$index.0"]);
+		"$acPMSIPIPGroupInDialogsTotal.$index.0",
+		"$acPMSIPIPGroupOutDialogsTotal.$index.0"]);
 
 	die $error if $error;
 	print pl2xml($test);
